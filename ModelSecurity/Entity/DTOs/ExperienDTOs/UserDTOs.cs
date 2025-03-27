9d4001b5e2654849742ce98021886a06221ed1b3
Maria <mariaalejan1080@gmail.com>
2025-03-27 00:18:59 -0500

    Context

---
+++ ModelSecurity/Entity/DTOs/ExperienDTOs/UserDTOs.cs
@@ -0,0 +1,25 @@
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTOs.ExperienDTOs
+{
+    class UserDTO
+    {
+        public int Id { get; set; }
+        public string name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+        
+    }
+}
+
+
+
+
+
